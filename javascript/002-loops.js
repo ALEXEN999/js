2252cc7cd34486027a733dd4b4f7f917eb72cbf0
ALEXEN999 <alexpf49@gmail.com>
2018-05-02 11:45:42 +0200

    .

---
+++ javascript/002-loops.js
@@ -0,0 +1,35 @@
+var elements = [3, 7, 9, 18, 24, 5, 7, 27, 32, 35, 42, 47];
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+function greaterThan18(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+
+  var total = 0;
+
+  for (i = 0; i < elements.length; i++) {
+   // Instrucciones
+    total = total + elements[i];
+  }
+
+  if (elements[i] < 18)
+
+  
+  alert(total);
+
+  console.log(person);
+  alert(person.olderThan18());
+
+  if (confirm('Eres mayor de 18?')) {
+      // Save it!
+      console.log("Congratulations");
+  } else {
+      // Do nothing!
+      console.log("Ya crecerás");
+  }
+
+
+}
+
+document.getElementById("result").innerHTML = greaterThan18(elements);
